04c625902e559d13fa93792e4bf4c4f5ae408543
prince antwi <antwi2k@gmail.com>
2021-06-22 14:23:25 +0800

    update App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import ClassComponet from './ClassComponent/ClassComponet';
+import FunctionComponent from './FunctionComponent/FunctionComponent';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <ClassComponet />
+     <hr />
+     <FunctionComponent />
+    </div>
+  );
+}
+
+export default App;
